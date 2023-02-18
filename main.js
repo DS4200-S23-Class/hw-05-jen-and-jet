066fef2ac352685509bc4c0b21a1e4200c6696f7
jethro-ronald-lee <99052024+jethro-ronald-lee@users.noreply.github.com>
2023-02-17 21:45:42 -0500

    got the bar chart to work :D

--- main.js
+++ main.js
@@ -1,26 +1,19 @@
 // Create a frame for the scatterplot
-const FRAME_HEIGHT = 550;
-const FRAME_WIDTH = 700; 
+const SCATTER_FRAME_HEIGHT = 550;
+const SCATTER_FRAME_WIDTH = 700; 
 
-// Set margins
-const MARGINS = {left: 50, right: 50, top: 50, bottom: 50};
+// Set scatter plot margins
+const SCATTER_PLOT_MARGINS = {left: 50, right: 50, top: 50, bottom: 50};
 
 // Set vis dimensions
-const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
-const VIS_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right; 
+const SCATTER_VIS_HEIGHT = SCATTER_FRAME_HEIGHT - SCATTER_PLOT_MARGINS.top - SCATTER_PLOT_MARGINS.bottom;
+const SCATTER_VIS_WIDTH = SCATTER_FRAME_WIDTH - SCATTER_PLOT_MARGINS.left - SCATTER_PLOT_MARGINS.right; 
 
 
 const FRAME1 = d3.select("#vis1")
                   .append("svg")
-                    .attr("height", FRAME_HEIGHT)
-                    .attr("width", FRAME_WIDTH)
-                    .attr("class", "frame"); 
-
-// Create a frame for the bar chart
-const FRAME2 = d3.select("#vis2")
-                  .append("svg")
-                    .attr("height", FRAME_HEIGHT)
-                    .attr("width", FRAME_WIDTH)
+                    .attr("height", SCATTER_FRAME_HEIGHT)
+                    .attr("width", SCATTER_FRAME_WIDTH)
                     .attr("class", "frame"); 
 
 
@@ -50,29 +43,29 @@ function build_interactive_plots() {
                       .domain([0, (MAX_Y1 + 1)])
                       .range([(MAX_Y1 * 50), 0]);
 
-    // Use X_SCALE to plot our points
+    // Use BAR_X_SCALE to plot our points
     FRAME1.selectAll("points")  
         .data(data) // passed from .then  
         .enter()       
         .append("circle")  
-          .attr("cx", (d) => { return (X1_SCALE(d.x) + MARGINS.left); }) 
-          .attr("cy", (d) => { return (Y1_SCALE(d.y) + MARGINS.top); }) 
+          .attr("cx", (d) => { return (X1_SCALE(d.x) + SCATTER_PLOT_MARGINS.left); }) 
+          .attr("cy", (d) => { return (Y1_SCALE(d.y) + SCATTER_PLOT_MARGINS.top); }) 
           .attr("r", 10)
           .attr("class", "point");
 
     // Add x axis to the vis  
     FRAME1.append("g") 
-          .attr("transform", "translate(" + MARGINS.left + "," + (VIS_HEIGHT + MARGINS.top) + ")") 
+          .attr("transform", "translate(" + SCATTER_PLOT_MARGINS.left + "," + (SCATTER_VIS_HEIGHT + SCATTER_PLOT_MARGINS.top) + ")") 
           .call(d3.axisBottom(X1_SCALE).ticks(4)) 
             .attr("font-size", '20px');
 
     // Add y axis
     FRAME1.append("g")       // g is a place holder for an svg
-          .attr("transform", "translate(" + MARGINS.left + "," + MARGINS.bottom + ")")
+          .attr("transform", "translate(" + SCATTER_PLOT_MARGINS.left + "," + SCATTER_PLOT_MARGINS.bottom + ")")
           .call(d3.axisLeft(Y1_SCALE).ticks(4))
             .attr("font-size", "20px");
 
-    function toggleBorder(event, d) {
+    function toggleBorder1(event, d) {
 
       // add or remove a border to a point if clicked on
       console.log(d.x)
@@ -89,6 +82,23 @@ function build_interactive_plots() {
       lastPointClickedDiv.innerHTML = lastPointClicked;
       }
     }
+
+    function toggleBorder2(event, d) {
+    
+      console.log('hola', this.classList)
+      // add or remove a border to a point if clicked on
+      if (Object.values(this.classList).includes('border')) {
+        this.classList.remove('border');
+      } else {
+        this.classList.add('border');
+      }
+
+      // show the coordinates of the last point clicked in the right column
+      let lastPointClicked = "Last Point Clicked: \n" + this.id;
+
+      let lastPointClickedDiv = document.getElementById("last-point-clicked");
+      lastPointClickedDiv.innerHTML = lastPointClicked;
+  }
 
 
     // Event handler for adding a point
@@ -107,11 +117,11 @@ function build_interactive_plots() {
 
       // create and set the attributes of the new element (point)
        FRAME1.append("circle")  
-              .attr("cx", (d) => { return (X1_SCALE(x_coord) + MARGINS.left); }) 
-              .attr("cy", (d) => { return (Y1_SCALE(y_coord) + MARGINS.top); }) 
+              .attr("cx", (X1_SCALE(x_coord) + SCATTER_PLOT_MARGINS.left))
+              .attr("cy", (Y1_SCALE(y_coord) + SCATTER_PLOT_MARGINS.top)) 
               .attr("r", 10)
-              .attr("class", "point")
-              .attr("onclick", "toggleBorder()")
+              .attr("class", "new-point")
+              .attr("id", '(' + x_coord + ', ' + y_coord + ')')
     }
 
     // get the button 
@@ -120,61 +130,65 @@ function build_interactive_plots() {
           .addEventListener('click', addPoint);
 
     // Add the event listeners that adds/removes a border to any point that gets clicked on
-      FRAME1.selectAll(".point")
-        .on('click', toggleBorder);
+    FRAME1.selectAll(".point")
+      .on('click', toggleBorder1);
+
+    // Add the event listeners that adds/removes a border to any point that gets clicked on
+    FRAME1.selectAll(".new-point")
+      .on('click', toggleBorder2);
     });
 
     // Bar chart
 
-    // set the dimensions and margins of the graph
-    var margin = {top: 30, right: 30, bottom:30, left: 60},
-        width = 460 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
+    // set the dimensions and SCATTER_PLOT_MARGINS of the graph
+    const BAR_CHART_MARGINS = {top: 30, right: 30, bottom:30, left: 60};
+    const BAR_FRAME_WIDTH = 460 - BAR_CHART_MARGINS.left - BAR_CHART_MARGINS.right;
+    const BAR_FRAME_HEIGHT = 400 - BAR_CHART_MARGINS.top - BAR_CHART_MARGINS.bottom;
 
     // append the svg object to the body of the page
-    var svg = d3.select("#vis2")
+    const FRAME2 = d3.select("#vis2")
       .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
+        .attr("width", BAR_FRAME_WIDTH + BAR_CHART_MARGINS.left + BAR_CHART_MARGINS.right)
+        .attr("height", BAR_FRAME_HEIGHT + BAR_CHART_MARGINS.top + BAR_CHART_MARGINS.bottom)
       .append("g")
         .attr("transform",
-              "translate(" + margin.left + "," + margin.top + ")");
+              "translate(" + BAR_CHART_MARGINS.left + "," + BAR_CHART_MARGINS.top + ")");
 
     // Parse the Data
     d3.csv("data/bar-data.csv").then((data) => {;
 
     // X axis
-    var x = d3.scaleBand()
-      .range([ 0, width ])
+    const BAR_X_SCALE = d3.scaleBand()
+      .range([ 0, BAR_FRAME_WIDTH ])
       .domain(data.map(function(d) { return d.category; }))
       .padding(0.2);
-    svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x))
+    FRAME2.append("g")
+      .attr("transform", "translate(0," + BAR_FRAME_HEIGHT + ")")
+      .call(d3.axisBottom(BAR_X_SCALE))
       .selectAll("text")
         .attr("transform", "translate(-10,0)rotate(-45)")
         .style("text-anchor", "end");
 
-    // Add Y 
+    // Add Y axis
 
     // find max value
     const MAX_VAL = d3.max(data, (d) => { return parseInt(d.amount); });
 
-    var y = d3.scaleLinear()
+    const BAR_Y_SCALE = d3.scaleLinear()
       .domain([0, MAX_VAL])
-      .range([ height, 0]);
-    svg.append("g")
-      .call(d3.axisLeft(y));
+      .range([ BAR_FRAME_HEIGHT, 0]);
+    FRAME2.append("g")
+      .call(d3.axisLeft(BAR_Y_SCALE));
 
     // Bars
-    svg.selectAll("mybar")
+    FRAME2.selectAll("mybar")
       .data(data)
       .enter()
       .append("rect")
-        .attr("x", function(d) { return x(d.category); })
-        .attr("y", function(d) { return y(d.amount); })
-        .attr("width", x.bandwidth())
-        .attr("height", function(d) { return height - y(d.amount); })
+        .attr("x", function(d) { return BAR_X_SCALE(d.category); })
+        .attr("y", function(d) { return BAR_Y_SCALE(d.amount); })
+        .attr("width", BAR_X_SCALE.bandwidth())
+        .attr("height", function(d) { return BAR_FRAME_HEIGHT - BAR_Y_SCALE(d.amount); })
         .attr("class", "bar");
 
     
